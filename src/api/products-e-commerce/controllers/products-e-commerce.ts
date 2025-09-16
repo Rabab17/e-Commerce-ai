9174b8d1daca12bb6b2c5e50a707f4c4365fd0fe
rabab alaa <161600491+Rabab17@users.noreply.github.com>
2025-09-16 16:02:20 +0300

    add collection and field

---
+++ src/api/products-e-commerce/controllers/products-e-commerce.ts
@@ -0,0 +1,7 @@
+/**
+ * products-e-commerce controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::products-e-commerce.products-e-commerce');
---
+++ src/api/products-e-commerce/services/products-e-commerce.ts
@@ -0,0 +1,7 @@
+/**
+ * products-e-commerce service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::products-e-commerce.products-e-commerce');
