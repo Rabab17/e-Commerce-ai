a50873287d54bdcf0afea4f10343bba88e11a603
rabab alaa <161600491+Rabab17@users.noreply.github.com>
2025-09-25 13:23:56 +0300

    edit database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,8 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  // const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT');
 
   const connections = {
     mysql: {
@@ -22,22 +23,16 @@ export default ({ env }) => {
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    postgres: {
+   postgres: {
       connection: {
-          connectionString: env('DATABASE_URL'), 
+        // احذف هذا السطر: connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        // هذا الجزء يجب أن يكون دقيقاً لاتصال SSL
+        ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
